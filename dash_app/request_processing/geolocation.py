aab8f560a5e4713cb213b84ca66c46267e88b2ff
Marko Sahan <marko.sahan@gmail.com>
2019-12-04 15:13:52 +0100

    stable version

--- dash_app/request_processing/geolocation.py
+++ dash_app/request_processing/geolocation.py
@@ -1,11 +1,11 @@
 from request_processing import get_service
 from typing import Tuple, Optional, Union
-from dash_app.placesdata import Place
+from placesdata import Place
 
 
 class Geolocation(object):
 
-    _travel_modes = {'walking', 'bicycling', 'driving', 'transit'}
+    _travel_modes = {"walking", "bicycling", "driving", "transit"}
 
     def __init__(self, find_me: bool = True):
         self.gmap = get_service()
@@ -18,13 +18,12 @@ class Geolocation(object):
         return self._me
 
     def get_my_location(
-            self,
-            specific_address: Optional[str] = None
+        self, specific_address: Optional[str] = None
     ) -> Tuple[float, float]:
 
         if specific_address is None:
-            result = self.gmap.geolocate()['location']
-            coords = (result['lat'], result['lng'])
+            result = self.gmap.geolocate()["location"]
+            coords = (result["lat"], result["lng"])
         else:
             coords = self.get_place(specific_address)
 
@@ -35,41 +34,42 @@ class Geolocation(object):
         if isinstance(place, str):
             address = place
         else:
-            address = ', '.join([place.street, place.city, place.zipcode])
-        result = self.gmap.geocode(address)[0]['geometry']['location']
+            address = ", ".join([place.street, place.city, place.zipcode])
+        result = self.gmap.geocode(address)[0]["geometry"]["location"]
 
-        return result['lat'], result['lng']
+        return result["lat"], result["lng"]
 
     def get_distance(self, destination: Tuple[float, float]) -> float:
 
-        result = self.gmap.distance_matrix(
-            self.me,
-            destination,
-            mode='walking'
-        )["rows"][0]["elements"][0]["distance"]["value"]
+        result = self.gmap.distance_matrix(self.me, destination, mode="walking")[
+            "rows"
+        ][0]["elements"][0]["distance"]["value"]
 
         return result
 
-    def get_navigations(self,
-                        destination: Tuple[float, float],
-                        travel_mode: str = 'driving'):
+    def get_navigations(
+        self, destination: Tuple[float, float], travel_mode: str = "driving"
+    ):
 
         assert travel_mode in self._travel_modes
 
-        link = f'https://www.google.com/maps/dir/?api=1&' \
-               f'origin={",".join([str(item) for item in self.me])}&' \
-               f'destination={",".join([str(item) for item in destination])}&' \
-               f'travelmode={travel_mode}'
+        link = (
+            f"https://www.google.com/maps/dir/?api=1&"
+            f'origin={",".join([str(item) for item in self.me])}&'
+            f'destination={",".join([str(item) for item in destination])}&'
+            f"travelmode={travel_mode}"
+        )
 
         return link
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
-    os.chdir('..')
+
+    os.chdir("..")
     gl = Geolocation(find_me=True)
-    coordinates = gl.get_place(Place('', '', 'NTK', '', 'Prague'))
-    print(f'My coordinates: {gl.me}')
-    print(f'NTK coordinates: {coordinates}')
-    print(f'Distance to NTK: {gl.get_distance(coordinates)}')
-    print(f'Directions to NTK: {gl.get_navigations(destination=coordinates)}')
+    coordinates = gl.get_place(Place("", "", "NTK", "", "Prague"))
+    print(f"My coordinates: {gl.me}")
+    print(f"NTK coordinates: {coordinates}")
+    print(f"Distance to NTK: {gl.get_distance(coordinates)}")
+    print(f"Directions to NTK: {gl.get_navigations(destination=coordinates)}")
