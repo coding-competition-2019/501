b401fbd5807d29360af2825da986f8b7489603cc
Marko Sahan <marko.sahan@gmail.com>
2019-12-04 10:38:15 +0100

    draft version of table look up

--- dash/run_server.py
+++ dash/run_server.py
@@ -4,10 +4,15 @@ import dash_bootstrap_components as dbc
 
 from dash.dependencies import Input, Output
 from layout import navbar, body, dropdown
+from inputs import inputs
+from activities_table import create_table
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([navbar, dropdown, body])
+app.layout = html.Div([navbar, inputs, dropdown, body])
+
+
+test_table = {"swimming": ["1", "10"], "fitness": ["2"]}
 
 
 @app.callback(Output("item-clicks", "children"), [Input("dropdown-button", "n_clicks")])
@@ -15,6 +20,13 @@ def count_clicks(n):
     if n:
         return f"Button clicked {n} times."
     return "Button not clicked yet."
+
+
+@app.callback(
+    Output("activities-output", "children"), [Input("activities-input", "value")]
+)
+def output_text(value):
+    return create_table(test_table, value)
 
 
 if __name__ == "__main__":
