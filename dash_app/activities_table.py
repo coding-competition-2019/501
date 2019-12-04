42770b09ffd232a9645ba086ed7caf2f069962fc
Marko Sahan <marko.sahan@gmail.com>
2019-12-04 11:56:24 +0100

    places and refactoring

--- dash_app/activities_table.py
+++ dash_app/activities_table.py
@@ -3,10 +3,24 @@ import dash_html_components as html
 import utils
 
 from typing import Tuple
+from placesdata import Place
 
 
 def get_data() -> Tuple[dict, dict]:
     data = utils.read_json("data/places.json")
+    data = [
+        (
+            val["activities"],
+            Place(
+                val["name"],
+                val["url"],
+                val["address"]["street"],
+                val["address"]["zipCode"],
+                val["address"]["city"],
+            ),
+        )
+        for val in data["places"]
+    ]
     places = utils.read_json("data/places_transformed.json")
     return data, places
 
@@ -15,22 +29,28 @@ def create_table(activity: str) -> dbc.Table:
 
     data, all_places = get_data()
 
-    table_header = [html.Thead(html.Tr([html.Th("Activities"), html.Th("Places")]))]
+    table_header = [html.Thead(html.Tr([html.Th("All Activities"), html.Th("Places")]))]
 
-    body = (
-        [
-            html.Tr(
-                [
-                    html.Td(" ".join(data["places"][value]["activities"])),
-                    html.Td(data["places"][value]["name"]),
-                ]
+    table_data: list = []
+    if activity in all_places["activity_list"]:
+        for index, value in enumerate(all_places["places"][activity]):
+            ###
+            # Vlad Function here (returns coordinates and distance from us. Takes html.Td(data[value][1] <- This is a place)
+            ###
+            table_data.append(
+                html.Tr(
+                    [html.Td(", ".join(data[value][0])), html.Td(data[value][1].name),]
+                )
             )
-            for value in all_places["places"][activity]
-        ]
-        if activity in all_places["activity_list"]
-        else [html.Td("Activity not found"), html.Td("")]
+            if index == 5:
+                break
+    else:
+        table_data.extend([html.Td("Activity not found"), html.Td("")])
+
+    table_body = [html.Tbody(table_data)]
+
+    return dbc.Table(
+        table_header + table_body,
+        bordered=True,
+        style={"maxHeight": "300px", "overflowY": "scroll"},
     )
-
-    table_body = [html.Tbody(body)]
-
-    return dbc.Table(table_header + table_body, bordered=True)
